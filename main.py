fac0bf927c28e20b4c94e325ca41f16372c8bc36
Felipe Zanardi <ftzanardi@gmail.com>
2025-08-04 20:55:48 -0300

    final version

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 from ultralytics import YOLO
 
-DATA = r'C:\Users\Lider\PycharmProjects\PlateDetection\data' # Endereço do database
+#DATA = r'C:\Users\Felipe\PycharmProjects\PlateDetection\data' # Endereço (full path) do database
+DATA = 'data' # Endereço do database
 
-model = YOLO('yolo11n-cls.pt') # Carregar Modelo
+modeloBase = YOLO('yolo11n-cls.pt') # Carregar Modelo
 
-results = model.train(data=DATA, epochs=20, imgsz=96) # Treinar Modelo
-
-'''
-epochs: quanto maior, melhor
-
-imgsz: tamanho da imagem processada.
-    as imagens do database tem resolução 75x100,
-    imgsz=64 faz com que se perca uma parte de informação da imagem,
-    fazendo com que perca acurácia, porém tempo de treinamento é menor.
-    Usar imgsz foi ideal para não perder informação, sem aumentar muito
-    o tempo de treinamento
-    (aparentemente é recomendado esse valor ser um número múltiplo de 32)
-    (a partir do epoch 15/16, não apresenta grande melhora)
-'''
+results = modeloBase.train(data=DATA, epochs=20, imgsz=96) # Treinar Modelo
--- predict.py
+++ predict.py
@@ -1,17 +1,22 @@
 from ultralytics import YOLO
 import numpy as np
+from PIL import Image
 
-modelo = YOLO(r'runs\classify\train5\weights\best.pt') # Carregar modelo próprio
+modeloTreinado = YOLO(r'runs\classify\train5\weights\best.pt') # Carregar modelo próprio
 
-resultado = modelo(r'data\train\t\3491.png') # Imagem para identificar caractere
+# Imagem para identificar caractere
+resultado = modeloTreinado(r'data\train\t\3491.png')    # endereço da imagem teste
 
-print(resultado)                            # teste - ver dados
-
-names_dict = resultado[0].names
-
+names = resultado[0].names
 probs = resultado[0].probs.data.tolist()
 
-print(names_dict)                           # teste - ver dados
-print(probs)                                # teste - ver dados
+print("\nO caractere é: ", names[np.argmax(probs)]) # Output (classe com maior probabilidade)
 
-print("O caractere é: ", names_dict[np.argmax(probs)])
+# Gerar imagem com probabilidades do top5 (5 classe com maiores probabilidades)
+for i, r in enumerate(resultado):
+    im_bgr = r.plot()
+    im_rgb = Image.fromarray(im_bgr[..., ::-1])
+    
+    r.show() # Mostrar resultado
+    
+    r.save(filename=f"resultado{i}.jpg") # Salvar resultado
